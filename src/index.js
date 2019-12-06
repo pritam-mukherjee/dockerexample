6ce48e11ee97b9b41016abcc7bae18aeba4ab6b5
Pritam Mukherjee <mukherjeepritam02@gmail.com>
2019-12-06 17:02:59 +0530

    Added shit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let devname = "Pritam Mukherjee";
+
+var freakingdata = {
+    "godsname": "Pritam Mukherjee",
+    "godfather": "Pritam Mukherjee",
+    "Company": {
+        "name": "Baracuda Corporation",
+        "location": "united States",
+        "position": "The goddamn CEO"
+    }
+};
+
+for(i in freakingdata){
+    console.log(i);
+}
