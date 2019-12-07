8fee036602bb5a8766f8badb5eebc4cdbf294bd6
Pritam Mukherjee <mukherjeepritam02@gmail.com>
2019-12-07 13:38:07 +0530

    Nothing to say really

--- src/index.js
+++ src/index.js
@@ -14,4 +14,6 @@ for(i in freakingdata){
     console.log(i);
 }
 
+console.log("Enter your choice: 1)close the damn window\n2)choose 1");
+
 console.log("System terminated the program successfully!");
